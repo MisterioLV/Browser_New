9c394d44858647c0176f33040c5fdfd6f28a270c
Raitis Barons <raitisbarons@gmail.com>
2021-06-10 20:59:16 +0300

    Add project files.

---
+++ Browser/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CefSharp.Wpf;
+
+namespace Browser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> WebPages;
+        int Current = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            WebPages = new List<string>();
+            GoHome();
+        }
+
+        void GoHome()
+        {
+            AddresBar.Text = "www.google.com";
+            Chrome.Address = "www.Google.com";
+            WebPages.Add("www.google.com");
+        }
+
+        void LoadWebPages(string Link, bool addToList = true)
+        {
+            AddresBar.Text = Link;
+            Chrome.Address = Link;
+            
+            MenuItem items = new MenuItem();
+            items.Click += MenuClicked;
+            items.Header = Link;
+           
+            items.Width = 184;
+            Menu.Items.Add(items);
+
+            if (addToList)
+            {
+                Current++;
+                WebPages.Add(Link);
+            }
+        }
+
+        void ToggleWebPages(string Option)
+        {
+            if(Option== "→")
+            {
+                if ((WebPages.Count - Current - 1) != 0)
+                {
+                    Current++;
+                    LoadWebPages(WebPages[Current], false);
+                }
+            }
+
+            else
+            {
+                if((WebPages.Count + Current-1) >= WebPages.Count)
+                {
+                    Current--;
+                    LoadWebPages(WebPages[Current], false);
+                }
+            }
+        }
+
+        private void Button_Click(object Sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)Sender;
+            ToggleWebPages(btn.Content.ToString());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            LoadWebPages(WebPages[Current]);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            LoadWebPages(WebPages[0]);
+        }
+
+        private void MenuClicked(object sender, RoutedEventArgs e)
+        {
+            MenuItem item = (MenuItem)sender;
+            LoadWebPages(item.Header.ToString());
+        }
+
+        private void AddresBar_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                LoadWebPages(AddresBar.Text);
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            if (WebPages.Count != 0)
+            {
+                Menu.PlacementTarget = hBTN;
+                Menu.Placement = System.Windows.Controls.Primitives.PlacementMode.Bottom;
+                Menu.HorizontalOffset = -155;
+                Menu.IsOpen = true;
+            }
+            
+        }
+
+        private void Button_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}
